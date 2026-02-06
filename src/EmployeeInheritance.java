a3bfee33c20c7e7fc49d0bce76162a3037a95332
theertha-175 <tve24el010@cet.ac.in>
2026-02-06 10:42:18 +0530

    Update EmployeeInheritance.java

--- src/EmployeeInheritance.java
+++ src/EmployeeInheritance.java
@@ -1,64 +1,65 @@
 import java.util.Scanner;
 
 class Employee {
-	String name;
-	int age;
-	String phoneNumber;
-	String address;
-	int salary;
+    String name;
+    int age;
+    long phoneNumber;
+    String address;
+    int salary;
 
-	void printSalary() {
-		System.out.println(salary);
-	}
+    void printSalary() {
+        System.out.println(salary);
+    }
 }
 
 class Officer extends Employee {
-	String specialization;
+    String specialization;
 }
 
 class Manager extends Employee {
-	String department;
+    String department;
 }
 
-public class EmployeeInheritance { 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+public class EmployeeInheritance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		Officer officer = new Officer();
-		officer.name = sc.nextLine();
-		officer.age = Integer.parseInt(sc.nextLine());
-		officer.phoneNumber = sc.nextLine();
-		officer.address = sc.nextLine();
-		officer.salary = Integer.parseInt(sc.nextLine());
-		officer.specialization = sc.nextLine();
+        Officer officer = new Officer();
+        officer.name = sc.nextLine();
+        officer.age = sc.nextInt();
+        officer.phoneNumber = sc.nextLong();
+        sc.nextLine(); // consume newline
+        officer.address = sc.nextLine();
+        officer.salary = sc.nextInt();
+        sc.nextLine();
+        officer.specialization = sc.nextLine();
 
-		Manager manager = new Manager();
-		manager.name = sc.nextLine();
-		manager.age = Integer.parseInt(sc.nextLine());
-		manager.phoneNumber = sc.nextLine();
-		manager.address = sc.nextLine();
-		manager.salary = Integer.parseInt(sc.nextLine());
-		manager.department = sc.nextLine();
+        Manager manager = new Manager();
+        manager.name = sc.nextLine();
+        manager.age = sc.nextInt();
+        manager.phoneNumber = sc.nextLong();
+        sc.nextLine();
+        manager.address = sc.nextLine();
+        manager.salary = sc.nextInt();
+        sc.nextLine();
+        manager.department = sc.nextLine();
 
-		System.out.println("Officer:");
-		System.out.println(officer.name);
-		System.out.println(officer.age);
-		System.out.println(officer.phoneNumber);
-		System.out.println(officer.address);
-		System.out.println(officer.salary);
-		System.out.println(officer.specialization);
+        System.out.println("Officer:");
+        System.out.println(officer.name);
+        System.out.println(officer.age);
+        System.out.println(officer.phoneNumber);
+        System.out.println(officer.address);
+        officer.printSalary();
+        System.out.println(officer.specialization);
 
-		System.out.println("Manager:");
-		System.out.println(manager.name);
-		System.out.println(manager.age);
-		System.out.println(manager.phoneNumber);
-		System.out.println(manager.address);
-		System.out.println(manager.salary);
-		System.out.println(manager.department);
+        System.out.println("Manager:");
+        System.out.println(manager.name);
+        System.out.println(manager.age);
+        System.out.println(manager.phoneNumber);
+        System.out.println(manager.address);
+        manager.printSalary();
+        System.out.println(manager.department);
 
-		sc.close();
-	}
+        sc.close();
+    }
 }
-
-
-		
